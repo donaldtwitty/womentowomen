19b3aa27b9deb0de792b4c0b1a02b566b78c2dd1
donaldtwitty <donaldctwittyjr@outlook.com>
2023-11-09 22:05:06 -0600

    working on js

--- html/js/main.js
+++ html/js/main.js
@@ -1,12 +1,20 @@
-const typed = select('.typed')
-if (typed) {
-    let typed_strings = typed.getAttribute('data-typed-items')
-    typed_strings = typed_strings.split(',')
-    new Typed('.typed', {
-        strings: typed_strings,
-        loop: true,
-        typeSpeed: 100,
-        backSpeed: 50,
-        backDelay: 2000
-    });
-}
+(function () {
+    "use strict";
+
+    const select = (el) => document.querySelector(el);
+
+    const on = (type, el, listener) => select(el).addEventListener(type, listener);
+
+    const typed = select('.typed')
+    if (typed) {
+        let typed_strings = typed.getAttribute('data-typed-items')
+        typed_strings = typed_strings.split(',')
+        new Typed('.typed', {
+            strings: typed_strings,
+            loop: true,
+            typeSpeed: 100,
+            backSpeed: 50,
+            backDelay: 2000
+        });
+    }
+});
